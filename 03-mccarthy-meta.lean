652e8ea87f3a912a068504771939714f64a8bd01
Alex Patel <patelalex02@gmail.com>
2017-07-10 18:31:06 -0400

    print precession of mccarthy 91 values

--- 03-mccarthy-meta.lean
+++ 03-mccarthy-meta.lean
@@ -10,8 +10,13 @@ see https://en.wikipedia.org/wiki/McCarthy_91_function
 meta def mninetyone : ℕ → ℕ 
 | n := if n > 100 then n - 10 else mninetyone (mninetyone (n + 11))
 
+meta def print_mninetyones : ℕ → io unit
+| 0 := return ()
+| (succ n) := print_mninetyones n >> put_str ("m91(" ++ nat.to_string n ++ ") = " ++ nat.to_string (mninetyone n) ++ "\n")
+
 #eval mninetyone 1
 #eval mninetyone 49
 #eval mninetyone 100
 #eval mninetyone 1000
+#eval print_mninetyones 150
 #print axioms mninetyone
