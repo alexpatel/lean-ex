137133908265d59ec37e04dec6768d107d578c09
Alex Patel <patelalex02@gmail.com>
2017-07-10 17:59:50 -0400

    add first 32 square numbers example

---
+++ 02-hundred-squares.lean
@@ -0,0 +1,14 @@
+import system.io
+variable [io.interface]
+open io nat
+
+/-
+print square of every integer from n to 0
+-/
+def print_squares : ℕ → io unit
+| 0 := return ()
+| (succ n) := print_squares n >>
+              put_str (nat.to_string n ++ "^2 = " ++ nat.to_string (n * n) ++ "\n")
+
+#eval print_squares 32
+#print axioms print_squares
