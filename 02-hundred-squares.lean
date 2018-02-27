54e4b7c1de31f51010c6032c6d5524cdb9c5bc45
dunyroll <36324438+dunyroll@users.noreply.github.com>
2018-02-27 12:04:37 +0100

    Update 02-hundred-squares.lean
    Lean 3

--- 02-hundred-squares.lean
+++ 02-hundred-squares.lean
@@ -8,7 +8,7 @@ print square of every integer from n to 0
 def print_squares : ℕ → io unit
 | 0 := return ()
 | (succ n) := print_squares n >>
-              put_str (nat.to_string n ++ "^2 = " ++ nat.to_string (n * n)
+              put_str (to_string n ++ "^2 = " ++ to_string (n * n)
                        ++ "\n")
 
 #eval print_squares 32
